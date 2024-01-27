d0a904c3578e9aa95f972833ecd036918fbabcde
Santiago San Román <santi72alc@gmail.com>
2024-01-27 20:29:59 +0100

    Hasta punto 102.- Funciones con argumentos

--- 7.- Funciones/FuncArgumentos/Ejer_02.py
+++ 7.- Funciones/FuncArgumentos/Ejer_02.py
@@ -3,3 +3,12 @@ Ejercicio 2
 
 Escribir una función que reciba una muestra de números en una lista y devuelva su medida.
 """
+
+def medida(*lista):
+    longitudParam = len(lista)
+    print(f"Longitud de introducido es {longitudParam}")
+    return longitudParam
+
+
+# Prueba
+medida(23,3,4,"Hola", "adios", 5)
